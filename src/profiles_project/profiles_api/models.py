57fdd2d014411452baf2deb3115afd3afd446c22
Dario <14darge@gmail.com>
2018-10-21 21:40:48 -0500

    Enabled the Django admin for the user profile model

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -46,7 +46,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIERED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Used to get a users full name."""
